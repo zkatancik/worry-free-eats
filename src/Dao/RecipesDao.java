380a78a5b2e73a8e61f9bb13edabc589cc29b2ad
haoweisu <su.hao@husky.neu.edu>
2020-11-10 23:50:31 +0800

    add select & delete methods

--- src/Dao/RecipesDao.java
+++ src/Dao/RecipesDao.java
@@ -5,6 +5,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
 
 public class RecipesDao {
   protected ConnectionManager connectionManager;
@@ -42,6 +44,91 @@ public class RecipesDao {
       }
       if (insertStmt != null) {
         insertStmt.close();
+      }
+    }
+  }
+
+  public Recipes getRecipeById(int recipeId) throws SQLException {
+    String getRecipe = "SELECT RecipeId,RecipeName,ImageUrl FROM Recipe WHERE RecipeId=?;";
+    Connection connection = null;
+    PreparedStatement selectStmt = null;
+    ResultSet resultSet = null;
+    Recipes recipe = null;
+    try {
+      connection = connectionManager.getConnection();
+      selectStmt = connection.prepareStatement(getRecipe);
+      selectStmt.setInt(1, recipeId);
+      resultSet = selectStmt.executeQuery();
+      if (resultSet.next()) {
+        int resultRecipeId = resultSet.getInt("RecipeId");
+        String recipeName = resultSet.getString("RecipeName");
+        String imageUrl = resultSet.getString("ImageUrl");
+        recipe = new Recipes(resultRecipeId, recipeName, imageUrl);
+      }
+      return recipe;
+    } catch (SQLException e) {
+      e.printStackTrace();
+      throw e;
+    } finally {
+      if (connection != null) {
+        connection.close();
+      }
+      if (selectStmt != null) {
+        selectStmt.close();
+      }
+    }
+  }
+
+  public List<Recipes> getRecipesByKeyword(String keyword) throws SQLException {
+    String getRecipe = "SELECT RecipeId,RecipeName,ImageUrl FROM Recipe WHERE CONTAINS(RecipeName, ?);";
+    Connection connection = null;
+    PreparedStatement selectStmt = null;
+    ResultSet resultSet = null;
+    List<Recipes> resultList = new LinkedList<>();
+    try {
+      connection = connectionManager.getConnection();
+      selectStmt = connection.prepareStatement(getRecipe);
+      selectStmt.setString(1, keyword);
+      resultSet = selectStmt.executeQuery();
+      while (resultSet.next()) {
+        int recipeId = resultSet.getInt("RecipeId");
+        String recipeName = resultSet.getString("RecipeName");
+        String imageUrl = resultSet.getString("ImageUrl");
+        resultList.add(new Recipes(recipeId, recipeName, imageUrl));
+      }
+      return resultList;
+    } catch (SQLException e) {
+      e.printStackTrace();
+      throw e;
+    } finally {
+      if (connection != null) {
+        connection.close();
+      }
+      if (selectStmt != null) {
+        selectStmt.close();
+      }
+    }
+  }
+
+  public Recipes delete(Recipes recipe) throws SQLException {
+    String deleteRecipe = "DELETE FROM Recipes WHERE RecipeId=?;";
+    Connection connection = null;
+    PreparedStatement deleteStmt = null;
+    try {
+      connection = connectionManager.getConnection();
+      deleteStmt = connection.prepareStatement(deleteRecipe);
+      deleteStmt.setInt(1, recipe.getRecipeId());
+      deleteStmt.executeUpdate();
+      return null;
+    } catch (SQLException e) {
+      e.printStackTrace();
+      throw e;
+    } finally {
+      if(connection != null) {
+        connection.close();
+      }
+      if(deleteStmt != null) {
+        deleteStmt.close();
       }
     }
   }
